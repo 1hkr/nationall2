715c368e1424bcaeba9f9b478f0f78ce9763eeaa
lorene vignaud <lorene.vignaud@gmail.com>
2018-02-27 15:46:37 +0100

    In two colons

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,5 @@ Rails.application.routes.draw do
     resources :opinions, only: [:create, :update, :destroy]
   end
   devise_for :users
-  # resources :pages, only: [:show]
   root to: 'pages#home'
-  # namespace :api, defaults: { format: :json } do
-  #   namespace :v1 do
-  #     get 'articles/:article_id/reviews', to:'reviews#index'
-  #   end
-  # end
 end
