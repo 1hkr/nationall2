44a43bcda19ab3a601fc7808239f7e568aeebf7f
Takahiko Okada <takahikookada@gmail.com>
2018-02-28 13:02:09 +0100

    editing map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:home]
+
   def home
     @articles = Article.all
     @users = User.where.not(latitude: nil, longitude: nil)
@@ -6,13 +9,18 @@ class PagesController < ApplicationController
       {
         lat: user.latitude,
         lng: user.longitude,
+        draggable: true,
         icon: {
-          url: user.picture, #
+          url: user.picture,
           scaledSize: {
             height: 50,
             width: 50
           }
           },
+        shape: {
+          coords: [1, 1, 1, 20, 18, 20, 18, 1],
+          type: 'poly'
+        },
         # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
       }
     end
