47fbf80ab57241c53dfa39d748e72986283f5548
lorene vignaud <lorene.vignaud@gmail.com>
2018-03-05 11:58:03 +0100

    Fix display of quizz

--- app/controllers/temperature_prefs_controller.rb
+++ app/controllers/temperature_prefs_controller.rb
@@ -2,10 +2,11 @@ class TemperaturePrefsController < ApplicationController
   skip_after_action :verify_authorized
 
   def update
-    if current_user.update(temperature_pref: params[:user][:temperature_pref], answered: true)
+    if params[:user] && current_user.update(temperature_pref: params[:user][:temperature_pref], answered: true)
       redirect_to articles_path, notice: 'Your preferences were successfully saved !'
     else
-      render 'quizzes#new'
+      @categories_pref = CategoriesPref.new
+      redirect_to new_quiz_path, notice: '!! Your need to answer all questions please !!'
     end
   end
 end
