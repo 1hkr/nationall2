13d47b12473998fe56faee9f561aad2b097ee513
lorene vignaud <lorene.vignaud@gmail.com>
2018-03-07 20:11:14 +0100

    media js void

--- app/assets/javascripts/media.js
+++ app/assets/javascripts/media.js
@@ -1,12 +1,1 @@
 
-var playersrc = document.getElementById('player').src
-
-playersrc.hover ( playVideo, pauseVideo );
-
-function playVideo(e) {
-  this.get(0).play();
-}
-
-function pauseVideo(e) {
-  this.get(0).pause();
-}
