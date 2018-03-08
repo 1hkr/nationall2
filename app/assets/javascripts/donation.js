909916a7b8cc4aa2cb258658f615dbea01ac7268
lorene vignaud <lorene.vignaud@gmail.com>
2018-03-08 23:05:49 +0100

    delete donation.js

--- app/assets/javascripts/donation.js
+++
@@ -1,5 +0,0 @@
-//$("#new_donation").submit(function(event){
-//  console.log('donation');
- //  const donation = document.getElementById('donate-form');
- //  donation.insertAdjacentHTML('afterbegin', '<%= j render "articles/donation"%>');
- //});
