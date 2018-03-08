4a8a711a455b9901f63f40a00a3a8916833eedf7
lorene vignaud <lorene.vignaud@gmail.com>
2018-03-08 23:01:34 +0100

    JS for donation done

---
+++ app/assets/javascripts/donation.js
@@ -0,0 +1,5 @@
+//$("#new_donation").submit(function(event){
+//  console.log('donation');
+ //  const donation = document.getElementById('donate-form');
+ //  donation.insertAdjacentHTML('afterbegin', '<%= j render "articles/donation"%>');
+ //});
