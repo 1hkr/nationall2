152ff50b7cf14b2b5befa78f35e0b4281b969a3b
Alexandre Gorius <ihkr@orange.fr>
2018-04-24 17:16:47 +0200

    Deleted footer on map page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,5 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-  def skip_footer?
-    return true
-  end
 end
 
