de90530c98884918741ada03cd7ecf6cdcdec8da
Takahiko Okada <takahikookada@gmail.com>
2018-05-10 22:35:56 +0200

    modification due to the implementation of Algolia to the article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312153507) do
+ActiveRecord::Schema.define(version: 20180331154023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20180312153507) do
     t.float "longitude"
     t.boolean "answered", default: false
     t.boolean "admin", default: false
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
